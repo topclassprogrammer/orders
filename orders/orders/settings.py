f340af777f7cb136c94b83dd48656eb55db6e1b9
topclassprogrammer <topclassprogrammer@gmail.com>
2024-11-10 10:34:09 +0300

    added postgresql DSN

--- orders/orders/settings.py
+++ orders/orders/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = "orders.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'orders',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
