beb4dff7ead73fefc7f59cd4ae62924b94f56fcd
topclassprogrammer <topclassprogrammer@gmail.com>
2024-11-10 17:31:57 +0300

    added User model

--- orders/backend/models.py
+++ orders/backend/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
 
 
@@ -9,3 +10,17 @@ class RoleChoices(models.TextChoices):
 
 class Role(models.Model):
     name = models.CharField(choices=RoleChoices, unique=True)
+
+
+class User(AbstractBaseUser):
+    USERNAME_FIELD = "username"
+
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    username = models.CharField(max_length=32, unique=True, validators=[check_username])
+    password = models.CharField(max_length=32, validators=[check_password])
+    email = models.CharField(max_length=320, unique=True, validators=[check_email])
+    phone = models.CharField(max_length=19, validators=[check_phone])
+    role = models.ForeignKey("Role", on_delete=models.CASCADE, related_name="users")
+    created_at = models.DateTimeField(auto_now_add=True)
+
