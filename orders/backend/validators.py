3f40f5ff953eba4dd50fc2710ffb19a4ba0d8ddb
topclassprogrammer <topclassprogrammer@gmail.com>
2024-11-10 17:28:07 +0300

    added check_password validator

--- orders/backend/validators.py
+++ orders/backend/validators.py
@@ -3,8 +3,15 @@ from string import ascii_letters, digits, punctuation
 from django.core.exceptions import ValidationError
 
 USERNAME_CHARS = ascii_letters + digits + "-_ "
+PASSWORD_CHARS = ascii_letters + digits + punctuation
 
 
 def check_username(value):
     if len(value) < 8 or any(char not in USERNAME_CHARS for char in value):
         raise ValidationError("Not allowed char(s) in username field")
+
+
+def check_password(value):
+    if len(value) < 8 or any(char not in PASSWORD_CHARS for char in value):
+        raise ValidationError("Not allowed char(s) in password field")
+
