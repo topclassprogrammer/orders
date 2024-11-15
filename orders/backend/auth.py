393be21d9a33f2d01758a6fc2f8095459603df76
topclassprogrammer <topclassprogrammer@gmail.com>
2024-11-16 00:01:39 +0300

    added TokenAuthentication class

---
+++ orders/backend/auth.py
@@ -0,0 +1,26 @@
+import uuid
+
+from rest_framework import exceptions
+from rest_framework.authentication import BaseAuthentication
+
+from backend.models import AuthToken
+from backend.validators import check_uuid_token
+
+
+class TokenAuthentication(BaseAuthentication):
+    def authenticate(self, request):
+        token_str = request.META.get('HTTP_AUTHORIZATION')
+        if not token_str or not token_str.startswith('Token '):
+            raise exceptions.AuthenticationFailed('No token provided')
+
+        token_list = token_str.split(" ")
+        if len(token_list) != 2:
+            raise exceptions.AuthenticationFailed('Incorrect token string')
+        token = token_list[1]
+        if not check_uuid_token(token):
+            raise exceptions.AuthenticationFailed('Incorrect token value')
+        try:
+            token_in_db = AuthToken.objects.get(key=uuid.UUID(token))
+        except AuthToken.DoesNotExist:
+            raise exceptions.AuthenticationFailed('No token found for this user in DB')
+        return token_in_db.user, token_in_db
