6a2fbd5f542e35d4eee5e7b1f34302ed51c69891
topclassprogrammer <topclassprogrammer@gmail.com>
2024-11-17 14:14:09 +0300

    added activate() func method for Account(ViewSet) class

--- orders/backend/views.py
+++ orders/backend/views.py
@@ -7,8 +7,8 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
 
 from backend.auth import TokenAuthentication
-from backend.models import ConfirmRegistrationToken, User, AuthToken
-from backend.serializers import CreateAccountSerializer, LogInSerializer
+from backend.models import ConfirmRegistrationToken, User, AuthToken, ActivationToken
+from backend.serializers import CreateAccountSerializer, LogInSerializer, ActivationSerializer
 from backend.utils import hash_password, check_hashed_passwords
 
 
@@ -52,4 +52,22 @@ class Account(ViewSet):
         auth_token.delete()
         return Response({"status": True, "message": "You just logged out"})
 
-
+    @action(methods=['POST'], authentication_classes=[TokenAuthentication], detail=False, url_path='activate')
+    def activate(self, request):
+        serializer = ActivationSerializer(data=request.data)
+        if serializer.is_valid():
+            key = request.data['key']
+            user = request.user
+            try:
+                registration_token = ActivationToken.objects.get(key=uuid.UUID(key))
+            except ActivationToken.DoesNotExist:
+                return Response({"status": False, "message": "Activation token key not found in DB"}, status=status.HTTP_404_NOT_FOUND)
+            if registration_token.user != user:
+                return Response({"status": False, "message": "You cannot activate account that does not belong to you"}, status=status.HTTP_403_FORBIDDEN)
+            if user.is_active:
+                return Response({"status": False, "message": "User is already activated"}, status=status.HTTP_400_BAD_REQUEST)
+            user.is_active = True
+            user.save()
+            registration_token.delete()
+            return Response({"status": True, "message": "Account successfully activated"}, status=status.HTTP_200_OK)
+        return Response({"status": False, "message": f"Incorrect activation data: {serializer.errors}"}, status=status.HTTP_400_BAD_REQUEST)
