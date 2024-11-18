39df076a86043f264456d749898b35c793adc087
topclassprogrammer <topclassprogrammer@gmail.com>
2024-11-18 14:16:49 +0300

    added IsAdmin class in permissions.py

--- orders/backend/permissions.py
+++ orders/backend/permissions.py
@@ -1,6 +1,8 @@
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import BasePermission
 
+from backend import models
+
 
 class IsOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
@@ -9,3 +11,11 @@ class IsOwner(BasePermission):
         elif not hasattr(obj, 'user') and request.user != obj:
             raise PermissionDenied('You cannot modify account does not belong to you')
         return True
+
+
+class IsAdmin(BasePermission):
+    def has_permission(self, request, view):
+        if request.user.role.name != models.RoleChoices.ADMIN:
+            raise PermissionDenied('You cannot get or modify any roles because you do not have admin role')
+        return True
+
