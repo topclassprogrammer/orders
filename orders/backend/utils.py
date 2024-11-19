f53532e0d029da215bcbd0c190b9f525f346920c
topclassprogrammer <topclassprogrammer@gmail.com>
2024-11-19 19:10:26 +0300

    added get_auth_token() func in utils.py

--- orders/backend/utils.py
+++ orders/backend/utils.py
@@ -1,4 +1,7 @@
+import uuid
+
 import bcrypt
+from rest_framework.exceptions import ValidationError
 
 
 def hash_password(value):
@@ -12,5 +15,19 @@ def check_hashed_passwords(password, stored_hash):
     password = password.encode()
     stored_hash = stored_hash.encode()
     return bcrypt.checkpw(password, stored_hash)
+
+
+def get_auth_token(request):
+    from backend.models import AuthToken
+    token_header = request.META.get('HTTP_AUTHORIZATION')
+    if not token_header:
+        raise ValidationError("No token provided")
+    token_list = token_header.split(" ")
+    token = token_list[1]
+    try:
+        auth_token = AuthToken.objects.get(key=uuid.UUID(token))
+    except AuthToken.DoesNotExist:
+        raise ValidationError("Your token does not exist in DB")
+    return auth_token
 
 
