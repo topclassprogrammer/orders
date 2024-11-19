73fba95b9173d7db47f43c3c99df07a18089ef08
topclassprogrammer <topclassprogrammer@gmail.com>
2024-11-19 21:17:26 +0300

    added accept_orders() method for ShopView

--- orders/backend/views.py
+++ orders/backend/views.py
@@ -11,7 +11,8 @@ from backend.models import User, AuthToken, ActivationToken, PasswordResetToken,
 from backend.permissions import IsOwner, IsAdmin
 from backend.serializers import CreateAccountSerializer, LogInSerializer, ActivationSerializer, PasswordResetSerializer, \
     RoleSerializer, ShopSerializer
-from backend.utils import hash_password, check_hashed_passwords, get_success_response, get_fail_response, get_object
+from backend.utils import hash_password, check_hashed_passwords, get_success_response, get_fail_response, get_object, \
+    get_auth_token
 
 
 class Account(ViewSet):
@@ -176,4 +177,13 @@ class ShopView(ViewSet):
         obj.delete()
         return Response(get_success_response(self.action, pk), status=status.HTTP_204_NO_CONTENT)
 
-
+    @action(methods=['POST'], detail=False, url_path="accept-orders")
+    def accept_orders(self, request):
+        auth_token = get_auth_token(request)
+        shop = Shop.objects.get(user__auth_tokens__in=[auth_token])
+        if shop.accept_orders:
+            shop.accept_orders = False
+        else:
+            shop.accept_orders = True
+        shop.save()
+        return Response({"status": True, "message": f"Accept orders was changed from {not shop.accept_orders} to {shop.accept_orders}"}, status=status.HTTP_200_OK)
