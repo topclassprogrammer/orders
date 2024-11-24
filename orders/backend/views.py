486b3a706c1e8e0d08c104f9bd26b6bfdd48227c
topclassprogrammer <topclassprogrammer@gmail.com>
2024-11-24 21:02:39 +0300

    added get_permissions() method for PropertyValueView

--- orders/backend/views.py
+++ orders/backend/views.py
@@ -449,7 +449,12 @@ class PropertyValueView(ModelViewSet):
         obj.delete()
         return Response(get_success_response(self.action), status=status.HTTP_204_NO_CONTENT)
 
-
+    def get_permissions(self):
+        if self.action == 'create':
+            return [HasShop()]
+        elif self.action in ['partial_update', 'destroy']:
+            return [IsOwner()]
+        return []
 
 
 
