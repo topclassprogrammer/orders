054aa8553787192bdaaae8775cbcdb00a9f1980a
topclassprogrammer <topclassprogrammer@gmail.com>
2024-11-24 21:42:40 +0300

    removed retrieve() method for ShopView

--- orders/backend/views.py
+++ orders/backend/views.py
@@ -166,12 +166,6 @@ class ShopView(ModelViewSet):
             return Response(get_success_response(self.action, serializer), status=status.HTTP_201_CREATED)
         return Response(get_fail_response(self.action, serializer), status=status.HTTP_400_BAD_REQUEST)
 
-    def retrieve(self, request, pk=None):
-        obj = get_object(Shop, pk)
-        serializer = ShopSerializer(obj)
-        return Response(get_success_response(self.action, serializer), status=status.HTTP_200_OK)
-
-
 
     def partial_update(self, request, pk=None):
         obj = get_object(Shop, pk)
