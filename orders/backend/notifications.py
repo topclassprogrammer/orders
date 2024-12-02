653d80475924e9e5ce84e693d772a265f47fe390
topclassprogrammer <topclassprogrammer@gmail.com>
2024-12-02 23:38:36 +0300

    added notifications.py

---
+++ orders/backend/notifications.py
@@ -0,0 +1,8 @@
+import os
+
+SMTP_SERVER = os.getenv('SMTP_SERVER')
+SMTP_PORT = int(os.getenv('SMTP_PORT'))
+EMAIL_ADDRESS = os.getenv('EMAIL_ADDRESS')
+EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
+
+
