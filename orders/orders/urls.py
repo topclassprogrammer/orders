c8d02e4e2bdb43719a5d16b8db6b691a8256e4c7
topclassprogrammer <topclassprogrammer@gmail.com>
2024-12-03 23:56:13 +0300

    edited urlpatterns in urls.py

--- orders/orders/urls.py
+++ orders/orders/urls.py
@@ -15,9 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
-from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+
+BASE_URL = 'api/v1/'
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-]
+    path('admin/', admin.site.urls),
+    path(BASE_URL, include('backend.urls'))
+] + debug_toolbar_urls()
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
