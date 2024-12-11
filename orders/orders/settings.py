a4ef14916da0211a6de2e247e09c564b0b2661c9
topclassprogrammer <topclassprogrammer@gmail.com>
2024-12-11 21:11:10 +0300

    added apps into INSTALLED_APPS list in settings.py

--- orders/orders/settings.py
+++ orders/orders/settings.py
@@ -31,17 +31,21 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+INTERNAL_IPS = ['127.0.0.1']
 
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "backend"
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'debug_toolbar',
+    'backend',
+    'rest_framework',
+    'django_filters'
 ]
 
 MIDDLEWARE = [
