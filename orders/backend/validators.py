764bcecb195b1ed1666e70922e2fe7bdbf9876e5
topclassprogrammer <topclassprogrammer@gmail.com>
2024-12-12 22:00:21 +0300

    added type annotations in validators.py

--- orders/backend/validators.py
+++ orders/backend/validators.py
@@ -15,28 +15,28 @@ USERNAME_CHARS = ascii_letters + digits + "-_ "
 PASSWORD_CHARS = ascii_letters + digits + punctuation + " "
 
 
-def check_username(value):
+def check_username(value: str):
     if len(value) < 4:
         raise ValidationError("Username must contain at least 4 chars")
     if any(char not in USERNAME_CHARS for char in value):
         raise ValidationError("Not allowed char(s) in username field")
 
 
-def check_password(value):
+def check_password(value: str):
     if len(value) < 8:
         raise ValidationError("Password must contain at least 4 chars")
     if any(char not in PASSWORD_CHARS for char in value):
         raise ValidationError("Not allowed char(s) in password field")
 
 
-def check_email(value):
+def check_email(value: str):
     pattern = re.compile(r"^[A-Za-z0-9._+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$")
     result = re.fullmatch(pattern, value)
     if not result:
         raise ValidationError("Incorrect email address")
 
 
-def check_phone(value):
+def check_phone(value: str):
     pattern = re.compile(r"^\+[0-9]{11,18}$")
     result = re.fullmatch(pattern, value)
     if not result:
@@ -50,7 +50,7 @@ def check_url(value: str):
         raise ValidationError("Incorrect shop URL")
 
 
-def check_uuid_token(value: str):
+def check_uuid_token(value: str) -> bool:
     try:
         uuid.UUID(value)
     except ValueError:
