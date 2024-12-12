253d539c5ea39218eb0e49dd31f9f5e4b530f986
topclassprogrammer <topclassprogrammer@gmail.com>
2024-12-12 23:36:57 +0300

    added docstring in filters.py

--- orders/backend/filters.py
+++ orders/backend/filters.py
@@ -4,6 +4,15 @@ from backend.models import Item
 
 
 class ItemFiler(FilterSet):
+    """
+    Filter set for filtering items based on various attributes.
+
+    Attributes:
+        brand (CharFilter): Filter for items based on the brand name.
+        model (CharFilter): Filter for items based on the model name.
+        category (CharFilter): Filter for items based on the category name.
+        shop (CharFilter): Filter for items based on the shop name.
+    """
     brand = CharFilter(field_name='brand__name', lookup_expr='icontains')
     model = CharFilter(field_name='model__name', lookup_expr='icontains')
     category = CharFilter(field_name='category__name', lookup_expr='icontains')
