12fd7eda537fd3b7d1c8dfd2a936483d99300e3b
topclassprogrammer <topclassprogrammer@gmail.com>
2025-01-10 20:34:29 +0300

    edited DEFAULT_THROTTLE_CLASSES in settings.py

--- orders/orders/settings.py
+++ orders/orders/settings.py
@@ -143,8 +143,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_THROTTLE_CLASSES': [
-        'rest_framework.throttling.UserRateThrottle',
-        'rest_framework.throttling.AnonRateThrottle'
+        'backend.throttle.CustomAnonRateThrottle',
+        'backend.throttle.CustomUserRateThrottle'
     ],
     'DEFAULT_THROTTLE_RATES': {
         'user': '10/minute',
